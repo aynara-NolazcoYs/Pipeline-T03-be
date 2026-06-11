01f5e1c46d25d432f02cdbf59e7af8c4101f0149
PieroSotelo <piero.sotelo@vallegrande.edu.pe>
2026-06-10 19:36:23 -0500

    feat(category):Implementando el service de categoria

---
+++ src/main/java/vallegrande/edu/pe/losQueensAgro/service/CategoryService.java
@@ -0,0 +1,9 @@
+package vallegrande.edu.pe.losQueensAgro.service;
+
+import vallegrande.edu.pe.losQueensAgro.model.Category;
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> findAll();
+    
+}
